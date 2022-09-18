c245120bc7409cdb3c55cd171b1179558026ecce
Stefan Rehder <rehder@mirror-health.com>
2022-09-18 12:12:00 +0200

    error handling and post request added

--- Controllers/HeroesController.cs
+++ Controllers/HeroesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using dotnet6_simple_hero_api_example.Data;
 using dotnet6_simple_hero_api_example.Models;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace dotnet6_simple_hero_api_example.Controllers
 {
@@ -8,49 +9,107 @@ namespace dotnet6_simple_hero_api_example.Controllers
   [ApiController]
   public class HeroesController : ControllerBase
   {
-    private static HeroCollection heroes = new HeroCollection();
+    private static readonly HeroCollection heroes = new HeroCollection();
 
     [HttpGet]
-    public IEnumerable<Hero> GetHeroes()
+    [SwaggerOperation(Summary = "Get all heroes synchronously.")]
+    [SwaggerResponse(200, "Everything is OK.")]
+    [SwaggerResponse(500, "Something went wrong.")]
+    public ActionResult<IEnumerable<Hero>> GetHeroes()
     {
-      return heroes.GetHeroes();
+      try
+      {
+        return Ok(heroes.GetHeroes());
+      }
+      catch (Exception e)
+      {
+        return StatusCode(500, e.Message);
+      }
     }
 
     [HttpGet("{name}")]
+    [SwaggerOperation(Summary = "Get a specific hero.")]
+    [SwaggerResponse(200, "Everything is OK.")]
+    [SwaggerResponse(404, "Hero was not found.")]
     public ActionResult<Hero> GetHero(string name)
     {
-      var hero = heroes.GetHero(name);
-      if (hero == null)
-        return NotFound();
-      else 
+      try 
+      {
+        var hero = heroes.GetHero(name);
         return Ok(hero);
+      } 
+      catch (Exception e) 
+      {
+        return NotFound(e.Message);
+      }
     }
 
-    [HttpPut()]
-    public ActionResult PutHero([FromBody] Hero hero)
+    [HttpPost()]
+    [SwaggerOperation(Summary = "Create a new hero.")]
+    [SwaggerResponse(201, "Everything is OK.")]
+    [SwaggerResponse(400, "Bad Request. The request body could not be parsed as a Hero.")]
+    [SwaggerResponse(409, "Hero already exist.")]
+    public ActionResult CreateNewHero([FromBody] Hero hero)
     {
       if (hero == null)
         return BadRequest();
-      else
+
+      try
       {
-        heroes.PutHero(hero); 
+        heroes.PostHero(hero);
+        return Created(String.Format("/api/Heroes/{0}",hero.Name), hero);
+      }
+      catch (Exception e)
+      {
+        return Conflict(e.Message);
+      }
+    }
+
+    [HttpPut()]
+    [SwaggerOperation(Summary = "Update existing hero.")]
+    [SwaggerResponse(200, "Everything is OK.")]
+    [SwaggerResponse(400, "Bad Request. The request body could not be parsed as a Hero.")]
+    [SwaggerResponse(404, "Hero was not found.")]
+    public ActionResult UpdateHero([FromBody] Hero hero)
+    {
+      if (hero == null)
+        return BadRequest();
+
+      try
+      {
+        heroes.PutHero(hero);
         return Ok();
+      }
+      catch (Exception e)
+      {
+        return NotFound(e.Message);
       }
     }
 
     [HttpDelete("{name}")]
+    [SwaggerOperation(Summary = "Delete existing hero.")]
+    [SwaggerResponse(200, "Everything is OK.")]
+    [SwaggerResponse(400, "Bad Request. The name parameter was empty.")]
+    [SwaggerResponse(404, "Hero was not found.")]
     public ActionResult DeleteHero(string name)
     {
       if (String.IsNullOrEmpty(name))
         return BadRequest();
-      else
+
+      try
       {
         heroes.DeleteHero(name);
         return Ok();
       }
+      catch (Exception e)
+      {
+        return NotFound(e.Message);
+      }
     }
 
+    // Example Async call - Imagine awaiting a call to a database instead of this memory fetch
     [HttpGet("Async")]
+    [SwaggerOperation(Summary = "Get all heroes asynchronously.")]
     public async IAsyncEnumerable<Hero> GetHeroesAsync()
     {
       foreach (var hero in await Task.Run(() => heroes.GetHeroes()))
--- Data/HeroCollection.cs
+++ Data/HeroCollection.cs
@@ -11,44 +11,67 @@ namespace dotnet6_simple_hero_api_example.Data
 
     public HeroCollection()
     {
-      // Always start with one Hero
+      // Always start app with these heroes
       AddHero(new Hero
       {
-          Name = "Super Woman",
-          Strength = 5
+        Name = "Super Woman",
+        Strength = 7
+      });
+      AddHero(new Hero
+      {
+        Name = "Batman",
+        Strength = 6
+      });
+      AddHero(new Hero
+      {
+        Name = "Superman",
+        Strength = 10
+      });
+      AddHero(new Hero
+      {
+        Name = "Robin",
+        Strength = 1
       });
     }
 
     public void AddHero(Hero hero)
     {
-      try
-      {
-          heroes.Add(hero.Name, hero);
-      }
-      catch (ArgumentException e)
-      {
-          // Hero already exists
-          // For now just write exception message to console and ignore
-          Console.WriteLine(e.Message);
-      }
+      if (heroes.ContainsKey(hero.Name))
+        throw new Exception("Hero already exists!");
+
+      heroes.Add(hero.Name, hero);
     }
 
-    public Hero GetHero(string name)
+    public Hero? GetHero(string name)
     {
+      if (!heroes.ContainsKey(name))
+        throw new Exception("Hero does not exist!");
+
       return heroes[name] as Hero;
     }
 
     public void PutHero(Hero hero)
     {
+      if (!heroes.ContainsKey(hero.Name))
+        throw new Exception("Hero does not exist! Create a new hero instead.");
+
+      DeleteHero(hero.Name);
+      AddHero(hero);
+    }
+
+    public void PostHero(Hero hero)
+    {
       if (heroes.ContainsKey(hero.Name))
-      {
-          DeleteHero(hero.Name);
-      }
+        throw new Exception("Hero already exists! Edit existing hero instead.");
+      
       AddHero(hero);
     }
 
     public void DeleteHero(string name)
     {
+      if (!heroes.ContainsKey(name))
+        throw new Exception("Hero does not exist!");
+
       heroes.Remove(name);
     }
 
